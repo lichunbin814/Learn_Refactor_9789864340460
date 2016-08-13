b78332094e4d0f847b0b60d4c7e8b65bce66c6c7
bin <bin@DESKTOP-S0ILSQS>
2016-08-13 22:55:18 +0800

    重構技巧：抽取類別(Extract Class) - 增加內聚性

---
+++ Learn_Refactor_9789864340460/Sample/Greeting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_Refactor_9789864340460.Sample
+{
+    public class Greeting
+    {
+        /// <summary>
+        /// 取得問候語
+        /// </summary>
+        private string GetGreeting(int hour)
+        {
+            string greeting = "";
+            if (hour >= 6 && hour <= 12)
+            {
+                greeting = "早安";
+            }
+            else if (hour >= 12 && hour <= 17)
+            {
+                greeting = "午安";
+            }
+            else
+            {
+                greeting = "晚安";
+            }
+
+            return greeting;
+        }
+
+        /// <summary>
+        /// 取得問候語
+        /// </summary>
+        public string GetGreeting(DateTime time, string user)
+        {
+            int hour = time.Hour;
+            string greeting = GetGreeting(hour);
+
+            return string.Format("Hi, {0}. {1}", user, greeting);
+        }
+    }
+}
--- Learn_Refactor_9789864340460/Sample/HelloWorld.cs
+++ Learn_Refactor_9789864340460/Sample/HelloWorld.cs
@@ -10,34 +10,9 @@ namespace Learn_Refactor_9789864340460.Sample
     {
         public string sayHello(DateTime time, string user)
         {
-            int hour = time.Hour;
-            string greeting = GetGreeting(hour);
-
-            return string.Format("Hi, {0}. {1}", user, greeting);
+            Greeting greeting = new Greeting();
+            return greeting.GetGreeting(time, user);
         }
-
-        /// <summary>
-        /// 取得問候語
-        /// </summary>
-        /// <param name="hour"></param>
-        /// <returns></returns>
-        private string GetGreeting(int hour)
-        {
-            string greeting = "";
-            if (hour >= 6 && hour <= 12)
-            {
-                greeting = "早安";
-            }
-            else if (hour >= 12 && hour <= 17)
-            {
-                greeting = "午安";
-            }
-            else
-            {
-                greeting = "晚安";
-            }
-
-            return greeting;
-        }
+        
     }
 }
