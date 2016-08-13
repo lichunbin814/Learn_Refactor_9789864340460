8a2386d421caee1cf91babb03490a9d385f41172
bin <bin@DESKTOP-S0ILSQS>
2016-08-13 22:50:52 +0800

    重構技巧：抽取方法(Extract Method)

--- Learn_Refactor_9789864340460/Sample/HelloWorld.cs
+++ Learn_Refactor_9789864340460/Sample/HelloWorld.cs
@@ -11,6 +11,18 @@ namespace Learn_Refactor_9789864340460.Sample
         public string sayHello(DateTime time, string user)
         {
             int hour = time.Hour;
+            string greeting = GetGreeting(hour);
+
+            return string.Format("Hi, {0}. {1}", user, greeting);
+        }
+
+        /// <summary>
+        /// 取得問候語
+        /// </summary>
+        /// <param name="hour"></param>
+        /// <returns></returns>
+        private string GetGreeting(int hour)
+        {
             string greeting = "";
             if (hour >= 6 && hour <= 12)
             {
@@ -25,7 +37,7 @@ namespace Learn_Refactor_9789864340460.Sample
                 greeting = "晚安";
             }
 
-            return string.Format("Hi, {0}. {1}", user, greeting);
+            return greeting;
         }
     }
 }
