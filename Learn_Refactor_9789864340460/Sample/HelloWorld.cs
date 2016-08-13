513e8ce1482cc3ea7c0611c28aa43869fa8e7992
bin <bin@DESKTOP-S0ILSQS>
2016-08-13 22:48:00 +0800

    新專範例專案：Hello World

---
+++ Learn_Refactor_9789864340460/Sample/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_Refactor_9789864340460.Sample
+{
+    public class HelloWorld
+    {
+        public string sayHello(DateTime time, string user)
+        {
+            int hour = time.Hour;
+            string greeting = "";
+            if (hour >= 6 && hour <= 12)
+            {
+                greeting = "早安";
+            }
+            else if (hour >= 12 && hour <= 17)
+            {
+                greeting = "午安";
+            }
+            else
+            {
+                greeting = "晚安";
+            }
+
+            return string.Format("Hi, {0}. {1}", user, greeting);
+        }
+    }
+}
