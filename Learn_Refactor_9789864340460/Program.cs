064bcaca8c8dd8c54cd83c6248a49be245342f4d
bin <bin@DESKTOP-S0ILSQS>
2016-08-13 22:35:40 +0800

    加入專案檔案。

---
+++ Learn_Refactor_9789864340460/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_Refactor_9789864340460
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+   
+}
